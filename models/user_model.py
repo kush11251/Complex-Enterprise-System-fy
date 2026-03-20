95cad866b643b35542777092dd333e2d88399654
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-20 18:40:57 +0530

    feat: integrate models/user_model.py

---
+++ models/user_model.py
@@ -0,0 +1,13 @@
+# Import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create the base
+Base = declarative_base()
+
+# Define the model
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
